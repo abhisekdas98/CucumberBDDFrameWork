d8024f46e78176881ecd72cd809d9ba3c17c2bb7
abhisekdas98 <abhisekdas98@gmail.com>
2023-01-12 20:51:37 +0530

    Added stepDefination class and POJO classes

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -1,4 +1,33 @@
 package stepDefinations;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class stepDefination {
+    @Given("Add Place Payload")
+    public void add_place_payload() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User Calls {string} with Post HTTP Request")
+    public void user_calls_with_post_http_request(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the API call got Success with status code {int}")
+    public void the_api_call_got_success_with_status_code(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("status in response body is {string}")
+    public void status_in_response_body_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("{string} in Response Body is {string}")
+    public void in_response_body_is(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
